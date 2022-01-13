b97a236a54a13238783aa44c4260ac9f60a1ad5e
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2022-01-13 14:20:04 +0530

    fix a tonna issues

--- Ports/freetype.sh
+++ Ports/freetype.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 unpack(){
     if ! [ -f cache/freetype-2.10.4.tar.gz ]; then # Check if tarball exists
         curl -Lo cache/freetype-2.10.4.tar.gz "https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.gz"
@@ -9,9 +10,9 @@ unpack(){
 buildp(){
  	cd $BUILD_DIR
  	patch -p1 < ../lemon-freetype-2.10.4.patch
- 	./configure --host=x86_64-lemon --prefix=$LEMON_PREFIX --with-harfbuzz=no --with-bzip2=no --disable-mmap --with-zlib=no --with-png=no --enable-shared --with-brotli=no
+ 	./configure --host=x86_64-lemon --prefix="$LEMON_PREFIX" --with-harfbuzz=no --with-bzip2=no --disable-mmap --with-zlib=no --with-png=no --enable-shared --with-brotli=no
  	make $JOBCOUNT
- 	make install DESTDIR=$LEMON_SYSROOT
-	ln -sf freetype2/freetype $LEMON_SYSROOT/$LEMON_PREFIX/include/
-	ln -sf freetype2/ft2build.h $LEMON_SYSROOT/$LEMON_PREFIX/include/
+ 	make install DESTDIR="$LEMON_SYSROOT"
+	ln -sf freetype2/freetype "$LEMON_SYSROOT/$LEMON_PREFIX/include/"
+	ln -sf freetype2/ft2build.h "$LEMON_SYSROOT/$LEMON_PREFIX/include/"
 }
